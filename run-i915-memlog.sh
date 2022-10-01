b1e93d9e427c591adfe76704e07251f3f40e0a8f
Ulisses Furquim <ulisses.furquim@intel.com>
2022-10-01 14:26:46 -0700

    Add total SMEM/LMEM interval tracking.
    - Every X ms (10 now) total smem and lmem are logged
    - Added pid object creation -> destruction tracking in bpftrace script
    - Moved each event/probe debug log under option

--- run-i915-memlog.sh
+++ run-i915-memlog.sh
@@ -7,9 +7,9 @@ function run_child()
 	exec ./valley_x64 -project_name Valley -data_path ../ -engine_config ../data/valley_1.0.cfg -system_script valley/unigine.cpp -sound_app openal -video_app opengl -video_multisample 1 -video_fullscreen 0 -video_mode 3 -extern_define ,RELEASE,LANGUAGE_EN,QUALITY_MEDIUM -extern_plugin ,GPUMonitor
 }
 
-run_child &
+run_child >& /dev/null &
 chid=$!
-KSRC=./rpmbuild/SOURCES/linux-5.19.8 ./i915-memlog.bt $! &
+KSRC=./rpmbuild/SOURCES/linux-5.19.8 ./i915-memlog.bt $! 10 &
 wait $chid
 sleep 10
 kill $!
