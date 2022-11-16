141886cf756b7c5864e3079c4f001046fdd9efa8
Ulisses Furquim <ulisses.furquim@intel.com>
2022-11-15 16:54:51 -0800

    Tweak sleep time in helper script.

--- i915-memlog/run-i915-memlog.sh
+++ i915-memlog/run-i915-memlog.sh
@@ -2,7 +2,7 @@
 
 function run_child()
 {
-	sleep 10
+	sleep 7
 	cd /home/ufurquim/Downloads/Unigine_Valley-1.0/bin
 	exec ./valley_x64 -project_name Valley -data_path ../ -engine_config ../data/valley_1.0.cfg -system_script valley/unigine.cpp -sound_app openal -video_app opengl -video_multisample 1 -video_fullscreen 0 -video_mode 3 -extern_define ,RELEASE,LANGUAGE_EN,QUALITY_MEDIUM -extern_plugin ,GPUMonitor
 }
@@ -11,5 +11,5 @@ run_child >& /dev/null &
 chid=$!
 KSRC=./linux-tree ./i915-memlog.bt $! 10 &
 wait $chid
-sleep 10
+sleep 3
 kill $!
